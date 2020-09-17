839dc1297d8e9a087918b2f5abaa957ab05f6424
kencale <kencale@gmail.com>
2020-09-16 20:22:01 -0400

    first commit of everything

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  MatSnackBarModule, MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule,
+  MatProgressBarModule, MatProgressSpinnerModule, MatCheckboxModule, MatSliderModule,
+  MatListModule,
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { TimeSeriesComponent } from './time-series/time-series.component';
+import { ChartService } from './shared/chart.service';
+import { HeadViewComponent } from './head-view/head-view.component';
+import { HeadsetInfoComponent } from './headset-info/headset-info.component';
+import { RecorderComponent } from './recorder/recorder.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimeSeriesComponent,
+    HeadViewComponent,
+    HeadsetInfoComponent,
+    RecorderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatListModule,
+  ],
+  providers: [
+    ChartService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
